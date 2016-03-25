b2758e809d3ddabcadcb4253d33bf7a6b2a7e4d7
Matthew Beatty <beattymg@gmail.com>
2016-03-24 22:04:45 -0400

    input generation

---
+++ inputgeneration.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+   // First flag is matrix dimension "n"
+   int dimension  = atoi(argv[1]);
+
+   // Type of input
+   // 1 -- 0, 1; 2 -- 0, 1, 2; 3 -- Random between 0 and 32;
+   int type = atoi(argv[2]);
+
+   FILE *f = fopen("matrix.txt", "w");
+   if (f == NULL)
+   {
+       printf("Error opening file!\n");
+       exit(1);
+   }
+
+   // for (int i = 0; i < dimension*4; i++) {
+   //    if (type == 1) {
+   //       // int r = rand() % 3;
+   //       // fprintf("%d\n", r);
+   //    }
+   //    else if (type == 2) {
+   //       fprintf("%d\n", i);
+   //    }
+   //    else {
+   //       fprintf("%d\n", i);
+   //    }
+   // }
+   
+   srand ( time(NULL) );
+
+   if (type == 1) {
+      for (int i = 0; i < dimension*4; i++) {
+         fprintf(f, "%d\n", rand() % 2);
+      }
+   }
+   else if (type == 2) {
+      for (int i = 0; i < dimension*4; i++) {
+         fprintf(f, "%d\n", rand() % 3);
+      }
+   }
+   else if (type == 3) {
+      for (int i = 0; i < dimension*4; i++) {
+         fprintf(f, "%d\n", rand() % 32);
+      }
+   }
+
+   
+   fclose(f);
+}
